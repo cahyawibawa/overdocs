8f933d76fd89455035a9a0432efeb21e07a13bbf
double fantasy <cahyawibawa.dev@gmail.com>
2024-05-03 16:43:14 +0700

    feat: update icons

--- components/layout/social-icon-button.tsx
+++ components/layout/social-icon-button.tsx
@@ -1,19 +1,17 @@
-import Link from 'next/link';
-
-import { Button } from '../ui/button';
-
-import type { Social } from '@/config/socials';
-import { Icons } from '../icons';
+import type { Social } from '@/config/socials'
+import Link from 'next/link'
+import { Icons } from '../icons'
+import { Button } from '../ui/button'
 
 export function SocialIconButton({ href, title, icon }: Social) {
-  const Icon = Icons[icon];
+  const Icon = Icons[icon]
 
   return (
-    <Button asChild size='icon' variant='outline'>
-      <Link href={href} target='_blank' rel='noreferrer'>
-        <span className='sr-only'>{title}</span>
-        <Icon className='h-4 w-4' />
+    <Button asChild size="icon" variant="outline">
+      <Link href={href} target="_blank" rel="noreferrer">
+        <span className="sr-only">{title}</span>
+        <Icon className="size-4" />
       </Link>
     </Button>
-  );
+  )
 }
