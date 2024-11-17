a5098bb03a58bb3a179f3b3bbcf68a66b98a3318
double fantasy <cahyawibawa.dev@gmail.com>
2024-11-17 22:11:33 +0700

    cleanup

--- lib/lucia/adapter.ts
+++
@@ -1,7 +0,0 @@
-import { DrizzlePostgreSQLAdapter } from "@lucia-auth/adapter-drizzle"
-import db from "../db"
-import { sessionTable, userTable } from "../db/schema"
-
-const adapter = new DrizzlePostgreSQLAdapter(db, sessionTable, userTable)
-
-export default adapter
--- types/fix.d.ts
+++
@@ -1,7 +0,0 @@
-import '@radix-ui/react-dialog';
-
-declare module '@radix-ui/react-dialog' {
-  export interface DialogPortalProps {
-    className?: string;
-  }
-}
