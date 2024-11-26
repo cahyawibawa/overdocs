655ff2772e349246abecfaf26a24662f3fced6ce
double fantasy <cahyawibawa.dev@gmail.com>
2024-11-26 23:56:05 +0700

    env

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-	baseURL: process.env.BETTER_AUTH_URL!,
+	baseURL: process.env.NEXT_PUBLIC_BASE_URL!,
 });
 
 export const { signIn, signUp, signOut, useSession } = authClient;
