fe4cbddd25f2ba1687bc265c620ac40340199d29
double fantasy <cahyawibawa.dev@gmail.com>
2024-11-26 23:38:43 +0700

    cleanup

--- lib/db/index.ts
+++
@@ -1,8 +0,0 @@
-import { neon } from "@neondatabase/serverless";
-import { type NeonHttpDatabase, drizzle } from "drizzle-orm/neon-http";
-import * as schema from "./schema";
-
-const sql = neon(process.env.DATABASE_URL!);
-const db = drizzle(sql, { schema }) as NeonHttpDatabase<typeof schema>;
-
-export default db;
