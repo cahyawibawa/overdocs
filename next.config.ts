2d9996e255333278d326dfd4e8a39a146bbac24c
cahya <cahyawibawa.dev@gmail.com>
2025-03-11 00:20:53 +0700

    chore: update biome & next config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "res.cloudinary.com",
+			},
+		],
+	},
 	experimental: {},
 };
 
