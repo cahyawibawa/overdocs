6e36ee7955297a76785bbc98194183b1b4806a6d
cahya <cahyawibawa.dev@gmail.com>
2025-03-11 22:24:23 +0700

    feat: improve email templates

--- components/emails/reset-password-email.tsx
+++ components/emails/reset-password-email.tsx
@@ -6,6 +6,7 @@ import {
 	Head,
 	Heading,
 	Html,
+	Img,
 	Link,
 	Preview,
 	Section,
@@ -15,65 +16,128 @@ import { Tailwind } from "@react-email/tailwind";
 
 interface ResetPasswordEmailProps {
 	url: string;
+	baseUrl?: string;
 	userFirstname?: string;
+	companyName?: string;
+	imageUrl?: string;
 }
 
 export default function ResetPasswordEmail({
 	url,
+	baseUrl,
 	userFirstname = "",
+	companyName = siteConfig.name,
+	imageUrl,
 }: ResetPasswordEmailProps) {
+	baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+	imageUrl = imageUrl || `${baseUrl}/vercel.svg`;
+	const previewText = "Reset your password";
 	return (
 		<Html>
-			<Head />
-			<Preview>Reset your password</Preview>
+			<Head>
+				<meta name="x-apple-disable-message-reformatting" />
+				<meta content="light dark" name="color-scheme" />
+				<meta content="light dark" name="supported-color-schemes" />
+
+				<style type="text/css">
+					{`
+                        :root {
+                            color-scheme: light dark;
+                            supported-color-schemes: light dark;
+                        }
+                    `}
+				</style>
+
+				<style type="text/css">
+					{`      
+                        html, body {
+                            background-color: #ffffff;
+                            color: #000000;
+                        }
+
+                        a {
+                            color: #000000;
+                        }
+
+                        .border-color {
+                            border-color: #eaeaea;
+                        }
+
+                        .action-button {
+                            background-color: #000000 !important;
+                            color: #ffffff !important;
+                        }
+
+                        @media (prefers-color-scheme: dark) {
+                            html, body {
+                                background-color: #000000 !important;
+                                color: #ffffff !important;
+                            }
+
+                            a {
+                                color: #ffffff;
+                            }
+
+                            .border-color {
+                                border-color: #333333 !important;
+                            }
+
+                            .action-button {
+                                background-color: rgb(38, 38, 38) !important;
+                                color: #ffffff !important;
+                            }
+                        }
+                    `}
+				</style>
+			</Head>
+			<Preview>{previewText}</Preview>
 			<Tailwind>
-				<Body className="bg-white font-sans">
-					<Container className="mx-auto px-10 py-20">
+				<Body className="mx-auto my-auto px-2 font-sans">
+					<Container className="mx-auto my-[40px] max-w-[465px] rounded border border-color border-solid p-5">
 						<Section className="mt-8">
-							<svg
-								width="76"
-								height="65"
-								viewBox="0 0 76 65"
-								fill="none"
-								xmlns="http://www.w3.org/2000/svg"
-							>
-								<path d="M37.5274 0L75.0548 65H0L37.5274 0Z" fill="#000000" />
-							</svg>
+							<Img
+								alt={companyName}
+								className="mx-auto my-0 rounded-full"
+								height="40"
+								src={imageUrl}
+								width="40"
+							/>
 
 							<Heading className="mb-8 text-center font-normal text-2xl">
 								Reset your password
 							</Heading>
 
 							{userFirstname && (
-								<Text className="text-gray-700">Hi {userFirstname},</Text>
+								<Text className="text-sm leading-6">Hi {userFirstname},</Text>
 							)}
 
-							<Text className="text-gray-700">
+							<Text className="text-sm leading-6">
 								Click the button below to reset your password. This link will
 								expire in 24 hours.
 							</Text>
 
-							<Section className="mt-8 mb-8 text-center">
+							<Section className="my-8 text-center">
 								<Button
-									className="box-border w-full rounded-[8px] bg-zinc-950 px-[12px] py-[12px] text-center font-semibold text-white"
+									className="action-button rounded px-5 py-3 text-center font-semibold text-xs no-underline"
 									href={url}
 								>
 									Reset Password
 								</Button>
 							</Section>
 
-							<Text className="text-gray-500 text-sm">
+							<Text className="text-sm leading-6">
 								If you didn't request a password reset, you can safely ignore
 								this email.
 							</Text>
 
-							<Text className="text-gray-500 text-sm">
-								Overdocs is built by{" "}
+							<Text className="text-sm">
+								Cheers
+								<br />
 								<Link
-									href="https://cahyawibawa.dev"
+									href="https://cahyawibawa.com"
 									className="text-blue-600 no-underline"
 								>
-									cahyawibawa.dev
+									cahyawibawa.com
 								</Link>
 							</Text>
 						</Section>
--- components/emails/verification-email.tsx
+++ components/emails/verification-email.tsx
@@ -1,3 +1,4 @@
+import { siteConfig } from "@/config/site";
 import {
 	Body,
 	Button,
@@ -15,41 +16,106 @@ import {
 
 interface VerificationEmailProps {
 	url: string;
+	baseUrl?: string;
 	userFirstname?: string;
 	companyName?: string;
+	imageUrl?: string;
 }
 
 export default function VerificationEmail({
 	url,
+	baseUrl,
 	userFirstname = "",
-	companyName = "Overdocs",
+	companyName = siteConfig.name,
+	imageUrl,
 }: VerificationEmailProps) {
+	baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+	imageUrl = imageUrl || `${baseUrl}/vercel.svg`;
 	const previewText = `Welcome to ${companyName}! Please verify your email address.`;
 	return (
 		<Html>
-			<Head />
-			<Preview>Verify your email address</Preview>
+			<Head>
+				<meta name="x-apple-disable-message-reformatting" />
+				<meta content="light dark" name="color-scheme" />
+				<meta content="light dark" name="supported-color-schemes" />
+
+				<style type="text/css">
+					{`
+                        :root {
+                            color-scheme: light dark;
+                            supported-color-schemes: light dark;
+                        }
+                    `}
+				</style>
+
+				<style type="text/css">
+					{`      
+                        html, body {
+                            background-color: #ffffff;
+                            color: #000000;
+                        }
+
+                        a {
+                            color: #000000;
+                        }
+
+                        .border-color {
+                            border-color: #eaeaea;
+                        }
+
+                        .action-button {
+                            background-color: #000000 !important;
+                            color: #ffffff !important;
+                        }
+
+                        @media (prefers-color-scheme: dark) {
+                            html, body {
+                                background-color: #000000 !important;
+                                color: #ffffff !important;
+                            }
+
+                            a {
+                                color: #ffffff;
+                            }
+
+                            .border-color {
+                                border-color: #333333 !important;
+                            }
+
+                            .action-button {
+                                background-color: rgb(38, 38, 38) !important;
+                                color: #ffffff !important;
+                            }
+                        }
+                    `}
+				</style>
+			</Head>
+			<Preview>{previewText}</Preview>
 			<Tailwind>
 				<Body className="mx-auto my-auto bg-white font-sans">
-					<Container className="mx-auto my-10 w-[465px] p-5">
+					<Container className="mx-auto my-[40px] max-w-[465px] rounded border border-color border-solid p-5">
 						<Section className="mt-8">
 							<Img
-								src={`${process.env.NEXT_PUBLIC_BASE_URL}/images/shadcn.jpg`}
+								alt={companyName}
+								className="mx-auto my-0 rounded-full"
+								height="40"
+								src={imageUrl}
 								width="40"
-								height="37"
-								alt="shadcn avatar"
-								className="mx-auto my-0"
 							/>
 						</Section>
 						<Heading className="mx-0 my-8 p-0 text-center font-normal text-2xl">
 							Welcome to <strong>{companyName}</strong>!
 						</Heading>
-						<Text className="text-center">
+
+						{userFirstname && (
+							<Text className="text-sm leading-6">Hi {userFirstname},</Text>
+						)}
+						<Text className="text-sm leading-6">
 							Please verify your email address by clicking the button below.
 						</Text>
-						<Section className="mt-[32px] mb-[32px] text-center">
+						<Section className="my-8 text-center">
 							<Button
-								className="rounded bg-[#000000] px-5 py-3 text-center font-semibold text-[12px] text-white no-underline"
+								className="action-button rounded px-5 py-3 text-center font-semibold text-xs no-underline"
 								href={url}
 							>
 								Verify Email
