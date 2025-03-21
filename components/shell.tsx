6b92559bd820591b958c709f836096ffb8d2688e
cahya <cahyawibawa.dev@gmail.com>
2025-03-21 23:32:40 +0700

    fix: adjust padding

--- components/shell.tsx
+++ components/shell.tsx
@@ -3,7 +3,7 @@ import type * as React from "react";
 
 import { cn } from "@/lib/utils";
 
-const shellVariants = cva("grid my-6 items-center px-2", {
+const shellVariants = cva("grid my-6 items-center px-4 lg:px-6", {
 	variants: {
 		variant: {
 			default: "container",
