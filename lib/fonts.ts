ba1cdf7db17e907bfa4330cb5f5897480d01efa7
cahya <cahyawibawa.dev@gmail.com>
2025-05-17 15:14:56 +0700

    feat: add Satoshi variable font

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,55 +1,24 @@
 import { cn } from "@/lib/utils";
-import {
-	Geist,
-	Geist_Mono,
-	Instrument_Sans,
-	Inter,
-	Mulish,
-	Noto_Sans_Mono,
-} from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import LocalFont from "next/font/local";
 
-const fontSans = Geist({
+const geistSans = Geist({
 	subsets: ["latin"],
-	variable: "--font-sans",
+	variable: "--font-geist-sans",
 });
 
-const fontMono = Geist_Mono({
+const geistMono = Geist_Mono({
 	subsets: ["latin"],
-	variable: "--font-mono",
+	variable: "--font-geist-mono",
 });
 
-const fontInstrument = Instrument_Sans({
-	subsets: ["latin"],
-	variable: "--font-instrument",
-});
-
-const fontNotoMono = Noto_Sans_Mono({
-	subsets: ["latin"],
-	variable: "--font-noto-mono",
-});
-
-const fontMullish = Mulish({
-	subsets: ["latin"],
-	variable: "--font-mullish",
-});
-
-const fontInter = Inter({
-	subsets: ["latin"],
-	variable: "--font-inter",
-});
-
-const fontServerMono = LocalFont({
-	src: "../fonts/ServerMono-Regular.otf",
-	variable: "--font-server-mono",
+const satoshi = LocalFont({
+	src: "../fonts/Satoshi-Variable.ttf",
+	variable: "--font-satoshi",
 });
 
 export const fontVariables = cn(
-	fontSans.variable,
-	fontMono.variable,
-	fontInstrument.variable,
-	fontNotoMono.variable,
-	fontMullish.variable,
-	fontInter.variable,
-	fontServerMono.variable,
+	geistSans.variable,
+	geistMono.variable,
+	satoshi.variable,
 );
